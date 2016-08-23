d2a75e01a9d151126550f53b2447a5b0ab94dd40
Alireza-S.N <alireza6677@gmail.com>
2016-08-23 17:53:23 +0430

    version 2 bugs fixed

--- size.sh
+++ size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-for i in "$@"; do
-    [ -e "$i" ] || continue
-    files=$files '$i'
-done
-size=`du -csh $files | tail -1 | cut -f 1`
+size=`du -csh "$@" | tail -1 | cut -f1`
 zenity --info --title "Actual size" --text "The actual size is: $size "
